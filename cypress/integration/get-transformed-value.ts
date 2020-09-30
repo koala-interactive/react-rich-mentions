fdd0717a8d7c9cf63e280cd522c470fbbc665228
Muzishell <adrienmuzika@gmail.com>
2020-09-30 18:04:46 +0200

    Simplify test for br linebreak in contentEditable

--- cypress/integration/get-transformed-value.ts
+++ cypress/integration/get-transformed-value.ts
@@ -48,13 +48,12 @@ context('Get transformed value', () => {
   });
 
   it('Should handle <br/> linebreak', () => {
-    cy.visit(Cypress.env('baseUrl') + '?hello%0Aworld');
-    cy.get('[data-cy=input]').type('{shift}{enter}.');
-    cy.get('[data-cy=input]').then($el =>
-      expect($el[0].innerText).to.eq('hello\nworld\n.')
-    );
+    cy.get('[data-cy=input]').then($el => {
+      $el.html('hello<br/>world.');
+      expect($el[0].innerText).to.eq('hello\nworld.');
+    });
     cy.get('[data-cy=parse]').click();
-    cy.get('[data-cy=result]').should('have.text', 'hello\nworld\n.');
+    cy.get('[data-cy=result]').should('have.text', 'hello\nworld.');
   });
 
   it('Should be able to clear', () => {
