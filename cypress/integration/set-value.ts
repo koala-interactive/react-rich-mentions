a82728d5b79ef0899d116726cdd4b3c5b39171d5
vthibault <vthibault.mobile@gmail.com>
2020-10-15 12:41:22 +0200

    🐛 Fixes CI build path

--- cypress/integration/set-value.ts
+++ cypress/integration/set-value.ts
@@ -5,15 +5,15 @@ context('config.customizeFragment', () => {
     cy.visit(Cypress.env('baseUrl'));
     cy.get('[data-cy=set-value]').click();
     cy.get('[data-cy=input]').then(elem => {
-      expect(elem.html().replace(/unicorn\.\w+\./, 'unicorn.')).equal(
-        '<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;&nbsp;&nbsp;&nbsp;<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;<img data-rich-mentions=":smile:" src="/unicorn.png" width="19" height="19" class="emojione vaM" data-integrity="">'
+      expect(elem.html().replace(/\/?unicorn\.\w+\./, 'unicorn.')).equal(
+        '<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;&nbsp;&nbsp;&nbsp;<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;<img data-rich-mentions=":smile:" src="unicorn.png" width="19" height="19" class="emojione vaM" data-integrity="">'
       );
     });
 
     cy.get('[data-cy=input]').type('_');
     cy.get('[data-cy=input]').then(elem => {
-      expect(elem.html().replace(/unicorn\.\w+\./, 'unicorn.')).equal(
-        '<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;&nbsp;&nbsp;&nbsp;<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;<img data-rich-mentions=":smile:" src="/unicorn.png" width="19" height="19" class="emojione vaM" data-integrity="">_'
+      expect(elem.html().replace(/\/?unicorn\.\w+\./, 'unicorn.')).equal(
+        '<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;&nbsp;&nbsp;&nbsp;<span data-rich-mentions=":smile:" class="emojione" style="font-size: 19px; line-height: 19px;" data-integrity="😉">😉</span>&nbsp;<img data-rich-mentions=":smile:" src="unicorn.png" width="19" height="19" class="emojione vaM" data-integrity="">_'
       );
     });
   });
