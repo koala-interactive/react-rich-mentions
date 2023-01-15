8090b97ee8d15833282c2d67d047f785ca8fb0c7
Jonathan Raiman <jonathanraiman@gmail.com>
2023-01-15 17:56:36 +0100

    bugfix

--- src/utils/handleFragmentEscape.ts
+++ src/utils/handleFragmentEscape.ts
@@ -24,6 +24,7 @@ export function handleFragmentEscape(
 
     if (!isValid) {
       event.preventDefault();
+      if (insertion == null) {return;}
       escapeFragmentWithValue(element, insertion);
     }
   }
